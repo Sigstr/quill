15c3a7f8062c3651af5c4fe993417c6f58d4ae70
Michael Auer <michael@sigstr.com>
2019-01-22 17:22:19 -0500

    Transpiling working finally

--- assets/js/dist/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-document.addEventListener('DOMContentLoaded', function () {
-  var pageTitle = document.getElementById('pageTitle').innerHTML;
-  pageTitle ? document.title = pageTitle + ' - Quill' : false;
-}, false);
--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,3 +1,9 @@
+'use strict';
+
+import getContrastRatio from 'get-contrast-ratio';
+
+console.log(getContrastRatio('white', 'black'));
+
 // Set page title
 document.addEventListener('DOMContentLoaded', () => {
     const pageTitle = document.getElementById('pageTitle').innerHTML;
