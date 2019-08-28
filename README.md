ecead48c091fb925b557fa389c0464703ab07d12
Michael Auer <miauer@icloud.com>
2019-08-28 16:38:53 -0400

    Refactor color components and add neutrals

--- README.md
+++
@@ -1,4 +0,0 @@
-# Quill
-
-## Overview
-Quill is Sigstr's design system. This is a website documenting the components.git 
--- color.js
+++ color.js
@@ -1,23 +1,48 @@
 import React from 'react'
 
-export const colors = {
-  sigstr: '4aa951',
-  onyx: '1B262B'
-};
-
 export const ColorBlock = ({
   hex,
   label
 }) => {
-  return (<div style={{
-    backgroundColor: '#' + hex
-  }}
-  >
-    {label}
-  </div>
+  return (
+    <div style={{
+      width: 160,
+      marginRight: 32,
+      marginBottom: 48
+    }}>
+      <div style={{
+        height: 96,
+        width: 160,
+        backgroundColor: '#' + hex,
+        marginBottom: 12,
+        borderRadius: 3
+      }}>
+      </div>
+      <div>{label}</div>
+      <div>{hex}</div>
+    </div>
   );
 };
 
+const colors = ({
+  onyx: '1B262B',
+  basalt: '3D4D54',
+  slate: '6A7173',
+  tufa: 'ACB3B6',
+  limestone: 'DEE1E3',
+  marble: 'ECECEC',
+  chalk: 'F2F4F5',
+  sigstr: '4aa951'
+})
+
 export const ColorGroup = () => {
-  return Object.entries(colors).map(([key, value]) => (<ColorBlock hex={value} label={key} />))
+  const blocks = Object.entries(colors).map(([key, value]) => (<ColorBlock hex={value} label={key} />));
+  return (
+    <div style={{
+      display: 'flex',
+      flexWrap: 'wrap'
+    }}>
+      {blocks}
+    </div>
+  );
 };
