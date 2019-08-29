f10c9648a8a8934fbe71f5ff146acf56b47f655b
Michael Auer <miauer@icloud.com>
2019-08-29 10:36:21 -0400

    Add all colors

--- color.js
+++ color.js
@@ -32,8 +32,21 @@ const colors = ({
   limestone: 'DEE1E3',
   marble: 'ECECEC',
   chalk: 'F2F4F5',
-  sigstr: '4aa951'
-})
+  blue: '76B8FF',
+  error: 'D13842',
+  graph_blue: '00CCFF',
+  graph_pink: 'FF6EE3',
+  graph_yellow: 'FFD26E',
+  leaf: '75C97B',
+  moss: '2E7533',
+  orange: 'FFAE76',
+  purple: '885FBD',
+  red: 'FF8276',
+  sigstr: '4aa951',
+  tag_blue: '005D7D',
+  tag_yellow: '856D39',
+  yellow: 'FFC74C',
+});
 
 export const ColorGroup = () => {
   const blocks = Object.entries(colors).map(([key, value]) => (<ColorBlock hex={value} label={key} />));
