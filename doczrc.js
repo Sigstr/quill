1d054262170ec742490850e7db99f2c17d7db877
Robbie Jacobs <robbie.jacobs@gmail.com>
2019-09-05 10:11:47 -0400

    attempting to allow for css and scss imports

--- doczrc.js
+++ doczrc.js
@@ -1,1 +1,17 @@
-export default {}
+export default {
+  onCreateWebpackChain: config => {
+    // Allow SCSS imports
+    config.module
+      .rule("scss")
+      .test(/\.css|scss|sass$/)
+      .use("style")
+      .loader("style-loader")
+      .end()
+      .use("css")
+      .loader("css-loader")
+      .end()
+      .use("sass")
+      .loader("sass-loader")
+      .end();
+  }
+};
