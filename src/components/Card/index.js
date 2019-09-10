5c233d6b7bcc5722d12890ff20b4a5735b40ecb2
Robbie Jacobs <robbie.jacobs@gmail.com>
2019-09-10 09:37:42 -0400

    Card with propTypes and defaultProps; card always has .card className

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import React from "react";
 
 import "./styles.css";
@@ -5,5 +6,13 @@ import "./styles.css";
 const Card = ({ children, className }) => (
   <div className={className}>{children}</div>
 );
+
+Card.propTypes = {
+  className: PropTypes.string
+};
+
+Card.defaultProps = {
+  className: "card"
+};
 
 export default Card;
