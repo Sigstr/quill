6f8d3a412e0e0ffaffb87d6d95e6bc8a58829e5e
Michael Auer <miauer@icloud.com>
2019-09-11 15:52:41 -0400

    generatebars function

--- src/components/Signal/index.js
+++ src/components/Signal/index.js
@@ -5,23 +5,36 @@ import React from "react";
 
 import "./styles.css";
 
-const Signal = ({ strength }) => (
+const MAX = 100;
+
+const generateBars = () => (
   <>
-    <div>
-      bars
-    </div>
-    <div>Strong</div>
+    <div>i</div>
+    <div>i</div>
+    <div>i</div>
+    <div>i</div>
+    <div>i</div>
   </>
 );
 
+const Signal = ({ value }) => {
+  // Takes in a score and renders bars based on that score
+  return (
+    <>
+      <div className="signal">{generateBars()}</div>
+      <div>Strong</div>
+    </>
+  );
+};
+
 Signal.propTypes = {
   showLabel: PropTypes.bool,
-  strength: PropTypes.number.isRequired
+  value: PropTypes.number.isRequired
 };
 
 Signal.defaultProps = {
   showLabel: true,
-  strength: 0,
+  value: 0,
 };
 
 export default Signal;
