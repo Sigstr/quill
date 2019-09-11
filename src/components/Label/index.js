6552f9a1936d5df0432cb7a8cbf1abac07264431
Michael Auer <miauer@icloud.com>
2019-09-11 14:26:58 -0400

    Add Label to exports

--- src/components/Label/index.js
+++ src/components/Label/index.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-import { Colors } from "../../foundations/Color/index.js"
+import { Colors } from "../../foundations/Color"
 
 import "./styles.css";
 
