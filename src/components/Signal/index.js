8416f03dc40b2eb44a8892ec69a266d187243927
Michael Auer <miauer@icloud.com>
2019-09-11 15:36:27 -0400

    stub out props

--- src/components/Signal/index.js
+++ src/components/Signal/index.js
@@ -1,17 +1,26 @@
 import PropTypes from "prop-types";
 import React from "react";
 
+// import Label from '../Label';
+
 import "./styles.css";
 
 const Signal = ({ strength }) => (
-  <div />
+  <>
+    <div>
+      bars
+    </div>
+    <div>Strong</div>
+  </>
 );
 
 Signal.propTypes = {
-  strength: PropTypes.number
+  showLabel: PropTypes.bool,
+  strength: PropTypes.number.isRequired
 };
 
 Signal.defaultProps = {
+  showLabel: true,
   strength: 0,
 };
 
