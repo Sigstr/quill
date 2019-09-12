affbd9fd2b5e67c955c53823a5c5b66fb8f9186c
Michael Auer <miauer@icloud.com>
2019-09-12 13:26:02 -0400

    trying something

---
+++ .docz/.cache/async-requires.js
@@ -0,0 +1,15 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/michael/Documents/GitHub/quill/.docz/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/michael/Documents/GitHub/quill/.docz/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-components-signal-signal-mdx": () => import("/Users/michael/Documents/GitHub/quill/src/components/Signal/signal.mdx" /* webpackChunkName: "component---src-components-signal-signal-mdx" */),
+  "component---src-home-mdx": () => import("/Users/michael/Documents/GitHub/quill/src/home.mdx" /* webpackChunkName: "component---src-home-mdx" */),
+  "component---src-components-card-card-mdx": () => import("/Users/michael/Documents/GitHub/quill/src/components/Card/card.mdx" /* webpackChunkName: "component---src-components-card-card-mdx" */),
+  "component---src-components-icon-button-icon-button-mdx": () => import("/Users/michael/Documents/GitHub/quill/src/components/IconButton/icon-button.mdx" /* webpackChunkName: "component---src-components-icon-button-icon-button-mdx" */),
+  "component---src-components-button-button-mdx": () => import("/Users/michael/Documents/GitHub/quill/src/components/button/button.mdx" /* webpackChunkName: "component---src-components-button-button-mdx" */),
+  "component---src-components-radio-radio-mdx": () => import("/Users/michael/Documents/GitHub/quill/src/components/radio/radio.mdx" /* webpackChunkName: "component---src-components-radio-radio-mdx" */),
+  "component---src-foundations-color-mdx": () => import("/Users/michael/Documents/GitHub/quill/src/foundations/color.mdx" /* webpackChunkName: "component---src-foundations-color-mdx" */)
+}
+
---
+++ .docz/doczrc.js
@@ -0,0 +1,12 @@
+export default {
+  base: "/quill/",
+  dest: "./docs",
+  menu: [
+    "Home",
+    {
+      menu: ["Color"],
+      name: "Foundations"
+    }
+  ],
+  src: "./src/"
+};
