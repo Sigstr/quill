7b5efeae1b5efaabd918cfd350238764981702b0
Michael Auer <miauer@icloud.com>
2019-09-12 09:34:19 -0400

    Generate description based on a numeric score

--- src/components/Signal/index.js
+++ src/components/Signal/index.js
@@ -5,7 +5,10 @@ import React from "react";
 
 import "./styles.css";
 
-const MAX = 100;
+const MAX_SCORE = 100;
+const LEVELS = 6;
+
+const INTERVAL = MAX_SCORE / (LEVELS - 1);
 
 const generateBars = () => (
   <>
@@ -17,24 +20,33 @@ const generateBars = () => (
   </>
 );
 
-const Signal = ({ value }) => {
-  // Takes in a score and renders bars based on that score
-  return (
-    <>
-      <div className="signal">{generateBars()}</div>
-      <div>Strong</div>
-    </>
-  );
-};
+const generateDescription = score => {
+  if (score == 0) return "None";
+  if (score <= INTERVAL) return "Weak";
+  if (score <= INTERVAL * 2) return "Below Average";
+  if (score <= INTERVAL * 3) return "Moderate";
+  if (score <= INTERVAL * 4) return "Good";
+  if (score <= INTERVAL * 5) return "Strong";
+  return;
+}
+
+// Takes in a score and renders bars based on that score
+const Signal = ({ score }) => (
+  <>
+    <div className="signal">{generateBars()}{score}</div>
+    {/* todo: use a label for the score label */}
+    <div>{generateDescription(score)}</div>
+  </>
+);
 
 Signal.propTypes = {
   showLabel: PropTypes.bool,
-  value: PropTypes.number.isRequired
+  score: PropTypes.number.isRequired
 };
 
 Signal.defaultProps = {
   showLabel: true,
-  value: 0,
+  score: 0,
 };
 
 export default Signal;
