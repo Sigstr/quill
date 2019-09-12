d0579841d8dacd182d1f21151e2bb973b5f79482
Michael Auer <miauer@icloud.com>
2019-09-12 09:59:43 -0400

    Refactor label generation to generate object with more usefule info about the signal

--- src/components/Signal/index.js
+++ src/components/Signal/index.js
@@ -2,42 +2,86 @@ import PropTypes from "prop-types";
 import React from "react";
 
 // import Label from '../Label';
+// import { Colors } from '../../foundations/color'
 
 import "./styles.css";
+
+// remove and import after label merge
+let Colors = {
+  onyx: "#000000"
+};
 
 const MAX_SCORE = 100;
 const LEVELS = 6;
 
 const INTERVAL = MAX_SCORE / (LEVELS - 1);
 
-const generateBars = () => (
-  <>
-    <div>i</div>
-    <div>i</div>
-    <div>i</div>
-    <div>i</div>
-    <div>i</div>
-  </>
-);
+const generateBars = () => {
 
-const generateDescription = score => {
-  if (score == 0) return "None";
-  if (score <= INTERVAL) return "Weak";
-  if (score <= INTERVAL * 2) return "Below Average";
-  if (score <= INTERVAL * 3) return "Moderate";
-  if (score <= INTERVAL * 4) return "Good";
-  if (score <= INTERVAL * 5) return "Strong";
-  return;
+  return (
+    <>
+      <div>i</div>
+      <div>i</div>
+      <div>i</div>
+      <div>i</div>
+      <div>i</div>
+    </>
+  );
+
+};
+
+const getScoreDetails = score => {
+  if (score === 0) {
+    return {
+      color: Colors.onyx,
+      description: "None",
+      strength: 0
+    };
+  } else if (score <= INTERVAL) {
+    return {
+      color: Colors.onyx,
+      description: "Weak",
+      strength: 1
+    };
+  } else if (score <= INTERVAL * 2) {
+    return {
+      color: Colors.onyx,
+      description: "Below Average",
+      strength: 2
+    };
+  } else if (score <= INTERVAL * 3) {
+    return {
+      color: Colors.onyx,
+      description: "Moderate",
+      strength: 3
+    };
+  } else if (score <= INTERVAL * 4) {
+    return {
+      color: Colors.onyx,
+      description: "Good",
+      strength: 4
+    };
+  } else {
+    // If score is greater than our INTERVAL * 4 always return strong
+    return {
+      color: Colors.onyx,
+      description: "Strong",
+      strength: 5
+    };
+  }
 }
 
 // Takes in a score and renders bars based on that score
-const Signal = ({ score }) => (
-  <>
-    <div className="signal">{generateBars()}{score}</div>
-    {/* todo: use a label for the score label */}
-    <div>{generateDescription(score)}</div>
-  </>
-);
+const Signal = ({ score }) => {
+  const scoreDetails = getScoreDetails(score);
+  return (
+    <>
+      <div className="signal">{generateBars()}{score}</div>
+      {/* todo: use a label for the score label */}
+      <div>{scoreDetails.description}</div>
+    </>
+  );
+}
 
 Signal.propTypes = {
   showLabel: PropTypes.bool,
