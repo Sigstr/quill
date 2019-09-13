d3351869c7c61c8da09140ec2d388598bdeca9f9
Michael Auer <miauer@icloud.com>
2019-09-13 09:38:30 -0400

    Fix colorgroup

--- src/foundations/Color/index.js
+++ src/foundations/Color/index.js
@@ -1,28 +1,4 @@
-// import React from "react";
-
-// const Colors = {
-//   basalt: "#3D4D54",
-//   blue: "#76B8FF",
-//   chalk: "#F2F4F5",
-//   error: "#D13842",
-//   graph_blue: "#00CCFF",
-//   graph_pink: "#FF6EE3",
-//   graph_yellow: "#FFD26E",
-//   leaf: "#75C97B",
-//   limestone: "#DEE1E3",
-//   marble: "#ECECEC",
-//   moss: "#2E7533",
-//   onyx: "#1B262B",
-//   orange: "#FFAE76",
-//   purple: "#885FBD",
-//   red: "#FF8276",
-//   sigstr: "#4aa951",
-//   slate: "#6A7173",
-//   tag_blue: "#005D7D",
-//   tag_yellow: "#856D39",
-//   tufa: "#ACB3B6",
-//   yellow: "#FFC74C"
-// };
+import React from "react";
 
 // NEUTRALS
 export const ONYX = "#1b262b";
@@ -50,44 +26,66 @@ export const TAG_BLUE = "#005d7d";
 export const YELLOW = "#ffc74c";
 export const TAG_YELLOW = "#856d39";
 
-// export const ColorBlock = ({ hex, label }) => {
-//   return (
-//     <div
-//       style={{
-//         width: 160,
-//         marginRight: 32,
-//         marginBottom: 48
-//       }}
-//     >
-//       <div
-//         style={{
-//           height: 96,
-//           width: 160,
-//           backgroundColor: hex,
-//           marginBottom: 12,
-//           borderRadius: 3
-//         }}
-//       ></div>
-//       <div>{label}</div>
-//       <div>{hex}</div>
-//     </div>
-//   );
-// };
+const Colors = {
+  basalt: BASALT,
+  blue: BLUE,
+  chalk: CHALK,
+  error: ERROR,
+  graph_blue: GRAPH_BLUE,
+  graph_pink: GRAPH_PINK,
+  graph_yellow: GRAPH_YELLOW,
+  leaf: LEAF,
+  limestone: LIMESTONE,
+  marble: MARBLE,
+  moss: MOSS,
+  onyx: ONYX,
+  orange: ORANGE,
+  purple: PURPLE,
+  red: RED,
+  sigstr: SIGSTR,
+  slate: SLATE,
+  tag_blue: TAG_BLUE,
+  tag_yellow: TAG_YELLOW,
+  tufa: TUFA,
+  yellow: YELLOW
+};
 
-// export const ColorGroup = () => {
-//   const blocks = Object.entries(Colors).map(([key, value]) => (
-//     <ColorBlock hex={value} label={key} />
-//   ));
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         flexWrap: "wrap"
-//       }}
-//     >
-//       {blocks}
-//     </div>
-//   );
-// };
+export const ColorBlock = ({ hex, label }) => {
+  return (
+    <div
+      style={{
+        width: 160,
+        marginRight: 32,
+        marginBottom: 48
+      }}
+    >
+      <div
+        style={{
+          height: 96,
+          width: 160,
+          backgroundColor: hex,
+          marginBottom: 12,
+          borderRadius: 3
+        }}
+      ></div>
+      <div>{label}</div>
+      <div>{hex}</div>
+    </div>
+  );
+};
 
-// export default Colors;
+export const ColorGroup = () => {
+  const blocks = Object.entries(Colors).map(([key, value]) => (
+    <ColorBlock hex={value} label={key} />
+  ));
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap"
+      }}
+    >
+      {blocks}
+    </div>
+  );
+};
