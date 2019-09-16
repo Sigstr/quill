7b8644f98e1d009ca26ea1bee5ae97aea22c81e3
Michael Auer <miauer@icloud.com>
2019-09-16 17:00:47 -0400

    Add detail to bundle and add missing semi

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { ONYX, SLATE } from "../../foundations/Color"
+import { ONYX, SLATE } from "../../foundations/Color";
 
 import "./styles.css";
 
