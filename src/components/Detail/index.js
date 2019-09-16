4ea2ee5124364e406e76ce134d34576c2a7b985a
Michael Auer <miauer@icloud.com>
2019-09-16 16:59:34 -0400

    Add className prop

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { ONYX, SLATE } from "../../foundations/Color"
 
@@ -6,11 +7,19 @@ import "./styles.css";
 
 import Label from '../Label';
 
-const Detail = ({ keyChildren, valueChildren }) => (
-  <div class="detail">
+const Detail = ({ className, keyChildren, valueChildren }) => (
+  <div className={`detail ${className}`}>
     <Label children={keyChildren} color={SLATE} />
     <Label children={valueChildren} color={ONYX} />
   </div>
 );
+
+Detail.propTypes = {
+  className: PropTypes.string,
+};
+
+Detail.defaultProps = {
+  className: ""
+};
 
 export default Detail;
