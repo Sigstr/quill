7aa5634104a45b6cb732a897d4c34a8f99574577
Michael Auer <miauer@icloud.com>
2019-09-16 11:33:24 -0400

    Fix merge conflict in components index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,6 @@
 export { Button } from "./Button";
 export { Card } from "./Card";
 export { IconButton } from "./IconButton";
-<<<<<<< HEAD
-export { Radio } from "./Radio";
-export { Signal } from "./Signal";
-=======
 export { Label } from "./Label";
 export { Radio } from "./Radio";
->>>>>>> master
+export { Signal } from "./Signal";
