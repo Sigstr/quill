91b9882c4d5f3d5526d10c0893d88e082e163b71
Michael Auer <miauer@icloud.com>
2019-11-20 09:51:21 -0500

    Refactor tag to use classnames

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -1,51 +1,18 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-import { BASALT, TUFA, GRAPH_BLUE, TAG_BLUE, SIGSTR, MOSS } from "../../foundations/Color";
-
 import "./styles.css";
 
-const getStyles = theme => {
-
-  let themeStyles = {};
-
-  // Defaults to a grey tag
-  switch (theme) {
-    case "blue":
-      themeStyles = {
-        color: TAG_BLUE,
-        backgroundColor: GRAPH_BLUE
-      };
-      break;
-
-    case "green":
-      themeStyles = {
-        color: MOSS,
-        backgroundColor: SIGSTR
-      };
-      break;
-
-    default:
-      themeStyles = {
-        color: BASALT,
-        backgroundColor: TUFA
-      };
-      break;
-  }
-
-  // Set background opacity to .24
-  themeStyles.backgroundColor += "24";
-
-  return themeStyles;
-};
-
-export const Tag = ({ theme, children }) => {
-  const styles = getStyles(theme);
-  return (
-    <div className="tag" style={{ color: styles.color, backgroundColor: styles.backgroundColor }}>{children}</div>
-  );
-};
+export const Tag = ({ className, children }) => (
+  <div className={className}>
+    {children}
+  </div>
+);
 
 Tag.propTypes = {
-  theme: PropTypes.string
+  className: PropTypes.string
 };
+
+Tag.defaultProps = {
+  className: ""
+}
