88c3cb92d4d1d52e048cde877e7ba091755a1f92
Michael Auer <miauer@icloud.com>
2020-02-10 15:12:34 -0500

    Partially abstract out nav items

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,26 +3,38 @@ import React from "react";
 
 import "./styles.css";
 
-export const Navigation = ({ children, className, disabled, onClick }) => (
-  <nav className="navigation">
-    <div>
-      <button className="appswitcher">
-        <div>
-          <img alt="T" src="" />
-          <span>v</span>
-        </div>
-        <div class="appswitcher-label">Email</div>
-      </button>
-      <a href="#" class="navigation-item">A</a>
-      <a href="#" class="navigation-item">B</a>
-      <a href="#" class="navigation-item">C</a>
-    </div>
-    <div>
-      <a href="#" class="navigation-item">H</a>
-      <button class="navigation-usermenu">U</button>
-    </div>
-  </nav>
-);
+// const NavigationItem = ({ icon, title, link, isExternal }) => {
+
+//   // Determine if this is the current page and flip state
+//   const isActive = true;
+//   return (
+//     <a className={`navigation-item ${isActive ? 'navigation-item-active' : null}`} href={link}>{icon}</a>
+//   );
+// };
+
+
+export const Navigation = ({ items }) => {
+  const navigationItems = items.map(item => <a href="#" class="navigation-item">{item.icon}</a>);
+  console.log(navigationItems);
+  return (
+    <nav className="navigation">
+      <div>
+        <button className="appswitcher">
+          <div>
+            <img alt="T" src="" />
+            <span>v</span>
+          </div>
+          <div class="appswitcher-label">Email</div>
+        </button>
+        {navigationItems}
+      </div>
+      <div>
+        <a href="#" class="navigation-item">H</a>
+        <button class="navigation-usermenu">U</button>
+      </div>
+    </nav>
+  );
+};
 
 Navigation.propTypes = {
   pages: PropTypes.array
